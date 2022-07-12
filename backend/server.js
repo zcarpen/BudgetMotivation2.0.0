6e8cdb1f684fb0e4a3cd1fa03b84833d855167ee
zcarpen <zcarpen@gmail.com>
2022-07-12 15:49:57 -0500

    adds JWT sign and cookie to client browser. Next need to create verification of JWT

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,7 @@ const PORT = process.env.PORT || 3002;
 const User = require('./db/schemas/Users');
 const Transaction = require('./db/schemas/Transactions')
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 
 const app = express();
 
@@ -20,17 +21,41 @@ mongoose.connect(
 
 // API Routes
 app.get('/login', async (req, res) => {
-    const user = await User.find({"username": "zcarpen"});
+    const user = await User.find({"username": "noemicarpen"});
+    const password = 'delete123'
+    console.log(process.env.SECRET)
 
-    // if user[0].password and enteredpassword match
-        // create new JWT
-        // put it in cookies
-        // redirect to main expense page
-    // else
-        // signal warning
-    const transactions = await Transaction.find({userID: user[0]._id.toString()})
-    res.send({user, transactions})
+    if (!user) {
+        return res.status(400).send('Cannot find user')
+    }
+
+    try {
+        console.log(user)
+        if (await bcrypt.compare(password, user[0].password)) {
+            const transactions = await Transaction.find({userID: user[0]._id.toString()})
+            const secret = process.env.SECRET;
+            console.log('this is the secret from the dotenv file ', secret)
+            const signedToken = jwt.sign({data: {userID: user[0]._id, username: user[0].username}}, secret )
+            console.log(signedToken)
+            res.cookie("accessToken", signedToken);
+            res.send({user, transactions})
+            // redirect to main expense page
+        } else {
+            res.status(400).send('password does not match')
+        }
+    } catch(error) {
+        console.log(error)
+    }
 })
+
+// function authenticateToken(req, res, next) {
+//     const authHeader = req.
+
+    // jwt.verify(token, process.env.SECRET, (err, user) => {
+    //     if (err) return res.sendStatus(403);
+    //     console.log(user)
+    // })
+// }
 
 app.post('/create-user', async (req, res) => {
     let user = {
@@ -42,13 +67,12 @@ app.post('/create-user', async (req, res) => {
     };
     
     try {
-        const salt = await bcrypt.genSalt();
-        const hashedPassword = await bcrypt.hash(user.password, salt)
+        const hashedPassword = await bcrypt.hash(user.password, 10)
         const hashedUser = new User({
             ...user, 
             password: hashedPassword,
         })
-        
+
         const result = await hashedUser.save();
         res.status(200).send('inserted new user')
     } catch(error) {
